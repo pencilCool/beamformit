4bc9ba2b4b92b4a7f6d4422cb1718531e477859a
tangyuhua <yhtang_xt@163.com>
2016-05-27 14:33:09 +0800

    Initial Commit

---
+++ beamformit/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  beamformit
+//
+//  Created by Tang yuhua on 16/5/27.
+//  Copyright © 2016年 Tang yuhua. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
